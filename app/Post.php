f997cd7d4dc2bb6dba11d2f79dd3d221ca9009db
fumihumi <bgts1260011@gn.iwasaki.ac.jp>
2018-01-16 11:15:10 +0900

    add action && view && routing

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    /**
+     * ブログポストのコメントを取得
+     */
+    public function Comments(){
+        // 投稿はたくさんのコメントを持つ(hasManyの第2引数が、post_idで、第3引数が、idの場合は省略することができます。)
+        return $this->hasMany('App\Comment', 'post_id');
+    }
+    /**
+     * ブログポストのカテゴリーを取得
+     */
+    public function Category(){
+        // 投稿は1つのカテゴリーに属する(belongsToの第2引数が、cat_idで、第3引数がidの場合は省略することができます)
+        return $this->belongsTo('App\Category', 'cat_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('bbc','PostsController@index')->name('bbc.index');

5c3d73e2bdc53eb47e208bd7d47b229b00f43b08
fumihumi <bgts1260011@gn.iwasaki.ac.jp>
2018-01-16 16:37:15 +0900

    fix routing

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// 追加
-/* リソースに対する様々なアクションを処理する、複数のルートがこの１定義により生成されます */
-Route::resource('bbc', 'PostsController');
-
 /* 掲示板一覧 */
 //Route::get('bbc','PostsController@index')->name('bbc.index');
-
 /* コメント一覧 */
 //Route::get('bbc/{id}', 'PostsController@show')->name('bbc.show');
-
 /* 投稿ページ */
 //Route::get('bbc/create','PostsController@create')->name('bbc.create');
 
+/* リソースに対する様々なアクションを処理する、複数のルートがこの１定義により生成されます */
+Route::resource('bbc', 'PostsController');
+
+Route::resource('comment', 'CommentsController');
+
+/* カテゴリー */
 Route::get('bbc/category/{id}', 'PostsController@showCategory')->name('bbc.showCategory');

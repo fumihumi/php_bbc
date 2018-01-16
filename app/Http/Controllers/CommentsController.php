71e348090663974e25bf6e9422a45ea2ead88ad5
fumihumi <bgts1260011@gn.iwasaki.ac.jp>
2018-01-16 16:44:47 +0900

    commentした際にcomment_countをカウントアップするように変更

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -6,18 +6,28 @@ use App\Comment;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CommentStoreRequest;
+use phpDocumentor\Reflection\Types\Null_;
 
 class CommentsController extends Controller
 {
     //
     public function store(CommentStoreRequest $request)
     {
-        $comment = new Comment;
-        $comment->post_id = $request->get('post_id');
-        $comment->commenter = $request->get('commenter');
-        $comment->comment = $request->get('comment');
-        $comment->save();
+        if($request != NULL or $request != "") {
+            $posts = DB::table('posts')->where('id', $request->get('post_id'))->first();
+            if($posts != NULL or $posts != "") {
+                $comment_count = $posts->comment_count + 1;
+            DB::table('posts')->where('id', $request->get('post_id'))->update(['comment_count' => $comment_count]);
+            }
 
-        return redirect()->back()->with('message', 'コメントを投稿しました。');
+            $comment = new Comment;
+            $comment->post_id = $request->get('post_id');
+            $comment->commenter = $request->get('commenter');
+            $comment->comment = $request->get('comment');
+            $comment->save();
+
+            return redirect()->back()->with('message', 'コメントを投稿しました。');
+        }
     }
+
 }
